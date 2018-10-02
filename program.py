5ba6c9d132344e5b41edfedf90f4b5e775601223
Emmanuel Karlsson <kaem16@nti.umea>
2018-10-02 11:20:48 +0200

    slut på lektionen tisdag

--- program.py
+++ program.py
@@ -1,6 +1,19 @@
-hjärter = 1
-ruter = 2
-klöver = 3
-spader = 4
+print("Välkommen till mitt spel av russian roulette. Vinn eller dö")
 
-print("Välkommen till mitt kortspel. Vinn eller dö")
+plats = input("välj ett nummer mellan 1 och 6: ")
+
+
+import random
+
+kula = [0,1,2,3,4,5]
+live = random.randint(0,5)
+kula[live] = "ded"
+for x in kula:
+  print(x)
+
+if int(plats) == live:  
+    print("Du är sämst på det här.")
+else:
+    print("Du är inte lika sämst som hen som dog")
+
+
---
+++ test.py
@@ -0,0 +1,9 @@
+
+#tal = 1,2,3,4,5,6
+kammare = random.randint(1,6)
+
+
+if kammare == 1:
+    print("Rest in peperonies.")
+else: 
+    print("Du överlevde denna gång.")
