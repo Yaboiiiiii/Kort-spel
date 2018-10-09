e62fb95a98266614f3c0f65d7fd1642e72826d14
Emmanuel Karlsson <kaem16@nti.umea>
2018-10-09 11:18:50 +0200

    blyat

--- program.py
+++ program.py
@@ -1,19 +1,22 @@
 print("Välkommen till mitt spel av russian roulette. Vinn eller dö")
 
-plats = input("välj ett nummer mellan 1 och 6: ")
+miss = 0
 
+plats = input("välj ett nummer mellan 0 och 5: ")
 
-import random
+while miss != 6:
 
-kula = [0,1,2,3,4,5]
-live = random.randint(0,5)
-kula[live] = "ded"
-for x in kula:
-  print(x)
+    import random
 
-if int(plats) == live:  
-    print("Du är sämst på det här.")
-else:
-    print("Du är inte lika sämst som hen som dog")
+    kula = [0,1,2,3,4,5]
+    live = random.randint(0,5)
+    kula[live] = "ded"
+    for x in kula:
+        print(x)
+
+    if int(plats) == live:  
+        print("Du är sämst på det här.")
+    else:
+        print("Du är inte lika sämst som hen som dog")
 
 
